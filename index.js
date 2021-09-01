e9ef167f1abd8ef829a32875ab05484844b06c0f
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-01 11:21:57 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+/*var bestCustomer = "not bob";*/
+
+function upperCaseCustomerName () {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "greg";
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "sam";
+}
